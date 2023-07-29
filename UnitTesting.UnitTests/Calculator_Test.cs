b0aa85cece1bd49faabf0b164024ef5b992ad57f
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-30 00:13:53 +0300

    Tests AAA

--- UnitTesting.UnitTests/Calculator_Test.cs
+++ UnitTesting.UnitTests/Calculator_Test.cs
@@ -28,5 +28,79 @@ namespace UnitTesting.UnitTests
             Assert.That(result, Is.Not.EqualTo(31));
 
         }
+
+        [Test]
+        public void Div_Double_Numbers()
+        {
+            // AAA
+            
+            // Arange
+            Calculator c= new Calculator();
+            // Act
+            double result =  c.Div_Double(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(.49).Within(.011) , "Not Equal");
+        }
+
+        [Test]
+        public void Div_By_Zero()
+        {
+            Calculator c=new Calculator();
+
+            //Assert.Throws<DivideByZeroException>(() =>
+            //{
+            //    c.Div_Double(1,0);
+            //});
+            Assert.That(() => c.Div_Double(.25, 0), Throws.TypeOf<DivideByZeroException>());
+        }
+
+        [Test]
+        public void Test_Collection_Items()
+        {
+            var arr = new[] { "saber", "maher", "Kayan" , "Fatema" , "Rahf" , "saber"};
+
+            //Assert.That(arr, Has.Exactly(5).Items);
+            //Assert.That(arr, Is.Unique);
+            //Assert.That(arr , Is.Not.Empty);
+            //Assert.That(arr , Does.Contain("saber"));
+            //Assert.That(arr , Has.Exactly(2).EqualTo("saber").And.Exactly(1).EqualTo("maher"));
+            Assert.That(arr, Has.Exactly(2).Matches<string>(s => s.Contains("sab")));
+        }
+
+        [Test]
+        public void Test_String() 
+        {
+            string s = "Eman";
+            Assert.That(s , Does.StartWith("E").And.EndWith("n"));
+        }
+
+        [Test]
+        public void Test_Boolean()
+        {
+            bool isFound = default;
+
+            Assert.That(isFound , Is.False);
+        }
+
+        [Test]
+        public void Test_Rang()
+        {
+            int i = 11;
+            Assert.That(i , Is.GreaterThan(10));
+            Assert.That(i , Is.GreaterThanOrEqualTo(10));
+            Assert.That(i, Is.InRange(10 , 20));
+        }
+
+        [Test]
+        public void Test_References()
+        {
+            Calculator c1 = new Calculator(); 
+            Calculator c3 = new Calculator();
+            Calculator c2 = c1;
+
+            //Assert.That(c1 , Is.SameAs(c3));
+            Assert.That(c1 , Is.SameAs(c2));
+        }
+
     }
 }
---
+++ UnitTesting.UnitTests/Product_Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.Project;
+
+namespace UnitTesting.UnitTests
+{
+    [TestFixture]
+    public class Product_Test
+    {
+        [Test]
+        public void Test_Find_Name_Property()
+        {
+            Product product = new Product();
+            Assert.That(product , Has.Property("Name").And.Not.Property("Age"));
+        }
+    }
+}
