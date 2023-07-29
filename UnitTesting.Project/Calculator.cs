fec8968a4942e6d5af76149301b7810715200114
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-29 23:22:56 +0300

    Add project files.

---
+++ UnitTesting.Project/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Project
+{
+    public class Calculator
+    {
+        public int Add_Int(int n1 , int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Sub_Int(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public int Div_Int(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+        public double Div_Double(double n1, double n2)
+        {
+            if (n2 == 0) throw new DivideByZeroException();
+            return n1 / n2;
+        }
+    }
+}
---
+++ UnitTesting.UnitTests/Calculator_Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.Project;
+
+namespace UnitTesting.UnitTests
+{
+    [TestFixture]
+    public class Calculator_Test
+    {
+        [Test]
+        public void Add_Int_Positive_Numbers()
+        {
+            //AAA
+
+            //Arrang
+            Calculator calc = new Calculator();
+
+            //Act
+            int result = calc.Add_Int(10, 20);
+            
+            //Assert
+            Assert.AreEqual(30 ,result);
+            Assert.That(result, Is.EqualTo(30));
+            Assert.That(result, Is.Not.EqualTo(31));
+
+        }
+    }
+}
