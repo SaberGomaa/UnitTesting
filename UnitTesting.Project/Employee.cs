8e97f1bfb42421c6830b560435840d27bc445091
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-30 12:38:29 +0300

    Dynamic Func For Test Cases

---
+++ UnitTesting.Project/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Project
+{
+    public class Employee
+    {
+        public int Age { get; set; }
+
+        public bool IsSeniorCitizen(int age)
+        {
+            return age >= 60 ;
+        }
+
+    }
+}
---
+++ UnitTesting.UnitTests/Employee_Test.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.Project;
+
+namespace UnitTesting.UnitTests
+{
+    [TestFixture]
+    public class Employee_Test
+    {
+        Employee emp;
+
+        [SetUp]
+        public void setup()
+        {
+            emp = new Employee();
+        }
+
+        //[Test]
+        //[Category("Senior Citizen")]
+        //public void TestEmployeeIsSeniorCitizen()
+        //{
+        //    var res = emp.IsSeniorCitizen(60);
+
+        //    Assert.IsTrue(res);
+        //}
+
+        //[Test]
+        //[Category("Not Senior Citizen")]
+        //public void TestEmployeeIsNotSeniorCitizen()
+        //{
+        //    var res = emp.IsSeniorCitizen(50);
+
+        //    Assert.IsFalse(res);
+        //}
+
+        [Test]
+        [TestCase(60 , true)]
+        [TestCase(70 , true)]
+        [TestCase(50 , false)]
+        public void TestEmployeeIsSeniorCitizen(int age , bool expectedValue)
+        {
+            var result = emp.IsSeniorCitizen(age);
+            Assert.AreEqual(expectedValue, result);
+        }
+        
+    }
+}
