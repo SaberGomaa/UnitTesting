150df7a87c7259260d2dbfea16a7676635030dda
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-30 15:47:12 +0300

    Test Case Source , and  read from Json file

---
+++ UnitTesting.UnitTests/EmployeeTestCaseData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.UnitTests
+{
+    public class EmployeeTestCaseData
+    {
+        public int age { get; set; }
+        public bool expectedResult { get; set; }
+    }
+}
--- UnitTesting.UnitTests/Employee_Test.cs
+++ UnitTesting.UnitTests/Employee_Test.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,15 +48,50 @@ namespace UnitTesting.UnitTests
         //    var result = emp.IsSeniorCitizen(age);
         //    Assert.AreEqual(expectedValue, result);
         //}
-        
+
+        //[Test]
+        //[TestCase(60 , ExpectedResult = true)]
+        //[TestCase(70 , ExpectedResult = true)]
+        //[TestCase(50 , ExpectedResult = false)]
+        //public bool TestEmployeeIsSeniorCitizen(int age)
+        //{
+        //    return emp.IsSeniorCitizen(age);
+        //}
+
+
         [Test]
-        [TestCase(60 , ExpectedResult = true)]
-        [TestCase(70 , ExpectedResult = true)]
-        [TestCase(50 , ExpectedResult = false)]
+        [TestCaseSource(typeof(TestData))]
         public bool TestEmployeeIsSeniorCitizen(int age)
         {
             return emp.IsSeniorCitizen(age);
         }
 
     }
+    public class TestData :IEnumerable
+    {
+        //public IEnumerator GetEnumerator()
+        //{
+        //    yield return new TestCaseData(50).Returns(false);
+        //    yield return new TestCaseData(40).Returns(false);
+        //    yield return new TestCaseData(60).Returns(true);
+        //    yield return new TestCaseData(70).Returns(true);
+        //}
+
+        public IEnumerator GetEnumerator()
+        {
+            StreamReader r = new StreamReader("E:\\c#\\C#ITI\\UnitTesting\\UnitTesting.UnitTests\\EmployeesData.json");
+            string json = r.ReadToEnd();
+
+            List<EmployeeTestCaseData> items =
+                JsonConvert.DeserializeObject<List<EmployeeTestCaseData>>(json);
+
+            foreach (var item in items)
+            {
+                yield return new TestCaseData(item.age).Returns(item.expectedResult);
+            }
+
+        }
+
+    }
+
 }
